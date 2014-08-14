5ccff57d1c1ae233165ef6cc5d7efac2cd43811a
Tomáš Blatný <blatny.tomas@seznam.cz>
2014-08-14 17:12:46 +0200

    Last fix of wikipedia source text

--- app/bot/TextParser/TextParser.php
+++ app/bot/TextParser/TextParser.php
@@ -68,7 +68,7 @@ class TextParser extends Object
 					return '_' . $text[0] . '_';
 				});
 				return strip_tags($text) . "\n" .
-					"-- from Wikipedia, free encyclopedia ( <https://en.wikipedia.org/wiki/$search|full article> )";
+					str_repeat(' ', 40)."-- from <https://en.wikipedia.org/|Wikipedia, free encyclopedia> ( <https://en.wikipedia.org/wiki/$search|full article> )";
 			} else {
 				return "I don't know, try http://lmgtfy.com/?q=$search.";
 			}

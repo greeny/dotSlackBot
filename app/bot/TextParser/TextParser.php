7d4d5ecc1b86ded1475961a0b16c7c4fe9082f5f
Tomáš Blatný <blatny.tomas@seznam.cz>
2014-08-14 17:02:31 +0200

    Trying to fix source link

--- app/bot/TextParser/TextParser.php
+++ app/bot/TextParser/TextParser.php
@@ -68,7 +68,7 @@ class TextParser extends Object
 					return '_' . $text[0] . '_';
 				});
 				return strip_tags($text) . "\n" .
-					"-- from Wikipedia, free encyclopedia (<https://en.wikipedia.org/wiki/$search|full article>)";
+					"-- from Wikipedia, free encyclopedia ( <https://en.wikipedia.org/wiki/$search|full article> )";
 			} else {
 				return "I don't know, try http://lmgtfy.com/?q=$search.";
 			}
